c09a8f31fe6b2a9f522683c9be3eb1b7c1e3c259
Nelson Kimani <nelsonkimani96@gmail.com>
2019-10-24 11:45:44 +0300

    Add database URL

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -11,7 +11,7 @@ public class DB {
 
         try {
             if (System.getenv("DATABASE_URL") == null) {
-                dbUri = new URI("postgres://localhost:5432/to_do");
+                dbUri = new URI("postgres://localhost:5432/wildlife_tracker");
             } else {
                 dbUri = new URI(System.getenv("DATABASE_URL"));
             }
